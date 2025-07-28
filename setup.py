5fd91db4ce6bddfd188a1a52471350d6a39b2912
Skarlett <Skarlett@users.noreply.github.com>
2025-07-28 12:43:13 -0500

    refactor

--- setup.py
+++ setup.py
@@ -6,5 +6,10 @@ setup(
     version='0.1.0',
     python_requires='>=3.2',
     packages=find_packages(),
-    scripts=["wg-name", "wg-whois"],
+    entry_points={
+        "console_scripts": [
+            "wg-names=wgname.name:main",
+            "wg-whois=wgname.whois:main",
+        ],
+    }
 )
